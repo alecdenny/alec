0003c25a39eb0fac99b2ea904feec5a0489018c3
alecdenny <54775672+alecdenny@users.noreply.github.com>
2021-04-14 13:07:06 -0700

    Update audiotest.js

--- audiotest.js
+++ audiotest.js
@@ -1,18 +1,18 @@
-let mySound;
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('ambient.mp3');
-}
+let song;
 
 function setup() {
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  text('tap here to play', 10, 20);
+  song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
+  createCanvas(720, 200);
+  background(255, 0, 0);
 }
 
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  mySound.play();
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
 }
