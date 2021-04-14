a9271dd66711328f0a570dddb92e21ad8fe04902
alecdenny <54775672+alecdenny@users.noreply.github.com>
2021-04-14 12:52:45 -0700

    Update audiotest.js

--- audiotest.js
+++ audiotest.js
@@ -1,18 +1,26 @@
-let mySound;
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('assets/doorbell');
-}
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/Pn1g1wjxl_0
+
+var song;
+var sliderRate;
+var sliderPan;
 
 function setup() {
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  text('tap here to play', 10, 20);
+  createCanvas(200, 200);
+  song = loadSound('rainbow.mp3', loaded);
+  song.setVolume(0.5);
+  sliderRate = createSlider(0, 1.5, 1, 0.01);
+  sliderPan = createSlider(-1, 1, 0, 0.01);
 }
 
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  mySound.play();
+function loaded() {
+  song.play();
+}
+
+function draw() {
+  background(random(255));
+  song.pan(sliderPan.value());
+  song.rate(sliderRate.value());
 }
